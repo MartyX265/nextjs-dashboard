12b0f22234ab535a6246723b370923d03e2d663c
Martin R <martinribotta6@gmail.com>
2024-08-23 17:17:43 -0300

    First Deploy

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Montserrat,Inter,Lusitana} from "next/font/google";
+export const montserrat = Montserrat({weight:['300','400','500','600'],subsets:["latin"]});
+export const inter = Inter({subsets:["latin"]});
+export const lusitana = Lusitana({subsets:["latin"],weight:["400","700"]})
